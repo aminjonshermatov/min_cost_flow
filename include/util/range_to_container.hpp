061ae1b43f2d0756ad089aa954e8b0abe7b20074
aminjonshermatov <aminjonshermatov@gmail.com>
2023-12-15 21:23:27 +0300

    ranges: ...

--- include/util/range_to_container.hpp
+++ include/util/range_to_container.hpp
@@ -8,7 +8,7 @@ namespace detail {
   template<typename Container, std::ranges::range R>
     requires std::convertible_to<std::ranges::range_value_t<R>, typename Container::value_type>
   Container operator|(R &&r, to_helper<Container>) {
-    return Container{r.begin(), r.end()};
+    return Container(r.begin(), r.end());
   }
 } // namespace detail
 
